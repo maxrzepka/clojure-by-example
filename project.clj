70e9dd77e18b9a0b7e5ff26606632d76f00cc21b
Maximilien Rzepka <maximilien.rzepka@gmail.com>
2012-10-21 16:48:20 +0200

    clean up dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject enliveByExample "1.0.0-SNAPSHOT"
   :description "Web App to play with enlive "
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring "1.1.0-beta2"]
-                 [org.clojars.sritchie09/enlive "1.2.0-alpha1"]
+                 [ring "1.0.2"]
+                 [enlive "1.0.1"]
                  [net.cgrand/moustache "1.1.0"]])
