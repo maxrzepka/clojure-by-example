31a5649fab63336745db60c877fb26829fbd4f52
Maximilien Rzepka <maximilien.rzepka@gmail.com>
2012-10-23 20:19:49 +0200

    upgrade deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject enliveByExample "1.0.0-SNAPSHOT"
-  :description "Web App to play with enlive "
+(defproject clojure-by-example "1.0.0-SNAPSHOT"
+  :description "Web App to play with clojure ecosystem : enlive "
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring "1.0.2"]
+                 [ring "1.1.0"]
                  [enlive "1.0.1"]
                  [net.cgrand/moustache "1.1.0"]])
