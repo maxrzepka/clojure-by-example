e2da8327d6cb391cfec56cfba80a8b6c57d68d2f
Maximilien Rzepka <maximilien.rzepka@gmail.com>
2013-04-07 03:14:13 +0200

    refactor for multiple projects

---
+++ enlive/project.clj
@@ -0,0 +1,8 @@
+(defproject clojure-by-example/enlive "1.0.0-SNAPSHOT"
+  :description "Web App to play with enlive"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring "1.1.0"]
+                 [enlive "1.0.1"]
+                 [net.cgrand/moustache "1.1.0"]]
+  :min-lein-version "2.0.0"
+  :main web.select)
--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject clojure-by-example "1.0.0-SNAPSHOT"
-  :description "Web App to play with clojure ecosystem : enlive "
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring "1.1.0"]
-                 [enlive "1.0.1"]
-                 [net.cgrand/moustache "1.1.0"]]
-  :min-lein-version "2.0.0"
-  :main web.select)
+  :description "Web Apps to play with clojure ecosystem : enlive, core.logic..."
+  :url "https://github.com/maxrzepka/clojure-by-example"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:url "git@github.com:maxrzepka/clojure-by-example.git"}
+  :sub ["enlive" "core.logic" "cascalog"]
+  :plugins [[lein-sub "0.2.4"]]
+  :aliases {"clean" ["sub" "clean"]})
