1883dd82b4e72608e5c0040adef2a1b0c5b1ca57
Maximilien Rzepka <maximilien.rzepka@gmail.com>
2013-05-16 21:49:35 +0200

    update enlive version

--- enlive/project.clj
+++ enlive/project.clj
@@ -2,7 +2,7 @@
   :description "Web App to play with enlive"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.1.0"]
-                 [enlive "1.0.1"]
+                 [enlive "1.1.1"]
                  [net.cgrand/moustache "1.1.0"]]
   :min-lein-version "2.0.0"
   :main web.select)
